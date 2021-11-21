b38c50a640dd688571ccc878fe162e85acf7e262
Brandon <Grant.b2134@gmail.com>
2021-11-21 15:55:45 -0500

    registered app/ created path

--- wow/wow/settings.py
+++ wow/wow/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookreview.apps.BookreviewConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
