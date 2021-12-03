69701073ed7ccd962cc99bc7bf820884f28c03c5
Ramsy Anton <anton.ra@hotmail.com>
2021-12-02 16:03:37 -0800

    Completed Models.py

--- wow/bookreview/models.py
+++ wow/bookreview/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=20)
+    genre_id = models.AutoField(primary_key=True)
+
 class Post(models.Model):
-    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=20)
+    review = models.CharField(max_length=1000)
     img_link = models.URLField()
-    title =  models.CharField(max_length=255)
-    body = models.TextField()
+    tags = models.CharField(max_length=20)
     date_created = models.DateTimeField()
+    post_id = models.AutoField(primary_key=True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     
-# creat a genre variable that would later be refrenced in the view.py that can be passed in the paramiters
+class SocialTag(models.Model):
+    name = models.CharField(max_length=20)
+    socialtag_id = models.AutoField(primary_key=True)
+
+class PostTag(models.Model):
+    PostTag_id = models.AutoField(primary_key=True)
+    socialtag_id = models.ForeignKey(SocialTag, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
