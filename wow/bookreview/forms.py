328e45188b8b3b534a1553bb6f40cf1b635eeeb9
Brandon <Grant.b2134@gmail.com>
2021-12-20 10:41:00 -0500

    updateds for tags/social links/templates

--- wow/bookreview/forms.py
+++ wow/bookreview/forms.py
@@ -1,12 +1,20 @@
+from datetime import datetime
 from django import forms
+from django.forms.widgets import  CheckboxSelectMultiple
+from .models import *
 
 
 class EditorForm(forms.Form):
     title = forms.CharField(max_length=255, required=True)
     img_link = forms.URLField(required=False)
     review = forms.CharField(widget=forms.Textarea, required=True)
-    twitter = forms.CharField(max_length=1000,required=False)
-    meta = forms.CharField(max_length=1000,required=False)
-    instagram = forms.CharField(max_length=1000,required=False)
-    linkedin = forms.CharField(max_length=1000,required=False)
+    instagram = forms.CharField(widget=forms.TextInput,required=False)
+    meta = forms.CharField(widget=forms.TextInput,required=False)
+    linkedin = forms.CharField(widget=forms.TextInput,required=False)
+    twitter = forms.CharField(widget=forms.TextInput,required=False)
+    date_created = datetime.now()
     
+    social = []
+    for tag in Tag.objects.all():
+        social.append((tag.socialtag_id, tag.name))
+    social = forms.MultipleChoiceField(widget=CheckboxSelectMultiple, choices=social, required=False)
