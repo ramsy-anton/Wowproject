b820bdf3313fb03b9f0f6ed28e37ecd1954af8d9
Ramsy Anton <anton.ra@hotmail.com>
2021-12-22 20:43:10 -0800

    Revised all templates, genre.py, and added static folder for CSS files

--- wow/bookreview/genre.py
+++ wow/bookreview/genre.py
@@ -3,12 +3,12 @@ genre1 = {
         "img_link":'https://media.istockphoto.com/photos/spooky-halloween-sky-picture-id1270121259?b=1&k=20&m=1270121259&s=170667a&w=0&h=LGJ9lGqFWSPCGUVS0hBRlTXXb_Ez2xZwWhmk5pccRZg='
         },
     "Fiction" :{
-        "img_link": "https://img.theculturetrip.com/450x/smart/wp-content/uploads/2021/03/2aycgej-e1617641233793.jpg"
+        "img_link": "https://www.pinclipart.com/picdir/big/88-886031_cards-transparent-spiderman-web-clipart.png"
         },
     "Anime" : {
-        "img_link" : "https://wallpapercave.com/wp/wp6106554.jpg"
+        "img_link" : "http://assets.stickpng.com/images/5852cd8a58215f0354495f6c.png"
         },
     "Non-Fiction": {
-        "img_link":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLA47jXtHICe47jDXHq2HFdekSco-U5ayoCC4v5e1islG_c8ePlRF5y5CKwnkSP3vb5eA&usqp=CAU"
+        "img_link":"http://clipart-library.com/img1/1110517.png"
         }
 }
